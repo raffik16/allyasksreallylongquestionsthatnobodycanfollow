38202ee612a0cbc594ef544097d83bd5109d8338
Raffi Keklikian <69125695+raffik16@users.noreply.github.com>
2024-02-09 13:28:57 -0800

    formatting

--- components/WordRevealer.vue
+++ components/WordRevealer.vue
@@ -1,35 +1,37 @@
 <template>
     <div>
         <transition name="fade">
-            <div v-if="!showText" class="word-revealer">
-                {{ currentWord }}
-            </div>
+            <div v-if="!showText" class="word-revealer" v-html="currentWord" />
         </transition>
 
         <transition name="fade">
             <div v-if="showText" class="kroq">
                 Famous words by Ally Johnson
+                <button v-if="showReplayButton" @click="replay" class="replay-btn">Replay</button>
+                <button v-if="showNextQuestionButton" @click="nextQuestion" class="next-question-btn">Next Question</button>
             </div>
         </transition>
-
     </div>
 </template>
+
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 
 const props = defineProps({
-    text: {
-        type: String,
+    texts: {
+        type: Array,
         required: true,
     },
 });
-
+let currentTextIndex = 0; // Index to track the current text being shown
 const currentWord = ref('');
-const words = props.text.split(' ');
+let words = props.texts[currentTextIndex]?.split(' ') || []; // Safely handle potential undefined value
 let index = 0;
 let intervalId;
 const showText = ref(false);
+const showReplayButton = ref(false);
+const showNextQuestionButton = ref(false); // New ref to control the visibility of the "Next Question" button
 
 const showNextWord = () => {
     if (index < words.length) {
@@ -37,10 +39,42 @@ const showNextWord = () => {
         index += 1;
     } else {
         clearInterval(intervalId);
-        // Use setTimeout to delay showing the next content
         setTimeout(() => {
-            showText.value = true; // This will now wait 1.25 second before setting to true
-        }, 1250);
+            showText.value = true;
+            showReplayButton.value = true;
+            if (currentTextIndex < props.texts.length - 1) {
+                showNextQuestionButton.value = true; // Show if there are more texts to display
+            }
+        }, 1000);
+    }
+};
+
+const replay = () => {
+    index = 0;
+    showText.value = false;
+    showReplayButton.value = false;
+    showNextQuestionButton.value = false;
+    currentWord.value = '';
+    intervalId = setInterval(showNextWord, 500);
+};
+
+const nextQuestion = () => {
+    if (currentTextIndex < props.texts.length - 1) {
+        currentTextIndex++;
+        index = 0; // Reset the index for the new text
+        // Immediately clear the current word display to avoid flash of previous text
+        currentWord.value = '';
+        // Prepare the words for the new text
+        words = props.texts[currentTextIndex].split(' ');
+        // Reset the display states
+        showText.value = false;
+        showReplayButton.value = false;
+        showNextQuestionButton.value = false;
+        // Clear any existing interval to avoid duplicates
+        clearInterval(intervalId);
+        // Delay the start of the next question to ensure a clean transition
+        // This also allows for any animations or transitions to complete
+        intervalId = setInterval(showNextWord, 500);
     }
 };
 
@@ -57,6 +91,8 @@ onUnmounted(() => {
 body,
 html {
     background: #eaf4fc;
+    font-family: 'Barlow', sans-serif;
+    font-weight: 400;
 }
 </style>
 <style scoped>
@@ -64,8 +100,6 @@ html {
     position: absolute;
     left: 50%;
     top: 50%;
-    font-family: 'Barlow', sans-serif;
-    font-family: 400;
     transform: translate(-50%, -50%);
     color: rgb(93, 168, 7);
     font-size: 100px;
@@ -75,17 +109,27 @@ html {
     position: absolute;
     left: 50%;
     top: 50%;
-
     transform: translate(-50%, -50%);
-    display: flex;
-    font-size: 24px;
-    font-family: 'Barlow', sans-serif;
-    color: rgb(93, 168, 7);
 
-    font-family: 400;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+
+    font-size: 24px;
+    color: rgb(93, 168, 7);
     text-align: center;
 }
 
+.replay-btn,
+.next-question-btn {
+    width: fit-content;
+    margin: 0 auto;
+    background: #91989e;
+    color: #FFF;
+    border: none;
+    padding: 8px 12px;
+    border-radius: 50px;
+}
 
 .fade-enter-active,
 .fade-leave-active {
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <word-revealer
-      text="It was kind of expensive right? Is it still? So I mean is it still I mean you've blown up so quickly. Is it still really nerve wracking even in front of audiences. And I know for comedians now, too, the social media is becoming such a bigger part of that whole routine as well. Like so many comedians now putting up clips and little things like snippets or crowd work or something like that, just to kind of blow up on TikTok, and then also that your show is selling out when it wasn't before because of the the stuff you're putting out on social media. Like, are people writing material specifically for social media now, or is it just, I'm gonna take these bits and pieces that are like, good enough to get people into the room, and that's what I'm gonna give them." />
-    <!-- <word-revealer text="This is a test string to be revealed word by word." /> -->
-
+    <word-revealer :texts="questions" />
   </div>
 </template>
+
+<script setup>
+
+// Example dynamic data, possibly could come from props, data, or a store
+const questions = ["It was kind of expensive right. Is it still. So I mean is it still I mean you've blown up so quickly. Is it still really nerve wracking even in front of audiences. And I know for comedians now, too, the social media is becoming such a bigger part of that whole routine as well. Like so many comedians now putting up clips and little things like snippets or crowd work or something like that, just to kind of blow up on TikTok, and then also that your show is selling out when it wasn't before because of the the stuff you're putting out on social media. Like, are people writing material specifically for social media now, or is it just, I'm gonna take these bits and pieces that are like, good enough to get people into the room, and that's what I'm gonna give them my gold?", "Fluffy. I'm noticing that even in this torrential downpour, this atmospheric river, this Pineapple Express, whatever it's been called, you're still wearing shorts. Will any whether you wear pants ever?"]
+</script>
