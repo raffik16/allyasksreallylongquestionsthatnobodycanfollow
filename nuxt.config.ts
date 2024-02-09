31f7a14bb5fb5a70eadaa18d6e2b29dac766e183
Raffi Keklikian <69125695+raffik16@users.noreply.github.com>
2024-02-09 13:32:47 -0800

    add meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          name: "description",
+          content: "The Official Wesbite for Ally Johnson's Questions",
+        },
+      ],
+    },
+  },
   routeRules: {
     // prerender index route by default
     "/": { prerender: true },
